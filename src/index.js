da214df9b9026de76ce06f1f0911e20b47eafe5a
Artur Yakhin <artur.yakhin52@gmail.com>
2022-10-05 18:13:16 +0500

    init

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import console from 'console';
+
+dotenv.config();
+
+const { PORT } = process.env;
+const { DATABASE_URL } = process.env;
+const app = express();
+
+const connectDataBase = async () => {
+  try {
+    mongoose.connect(DATABASE_URL);
+    console.log('Connecting to DataBase');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+mongoose.connection.on('disconnected', () => {
+  console.log('log: mongoDB disconnected');
+});
+mongoose.connection.on('connected', () => {
+  console.log('log: mongoDB connected');
+});
+
+// midlewares
+app.use(express.json());
+app.use(cors());
+
+// router
+app.get('/', (req, res) => {
+  res.json({
+    message: 'hi',
+  });
+});
+
+const startApp = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server lister on ${PORT}`);
+      connectDataBase();
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+startApp();
